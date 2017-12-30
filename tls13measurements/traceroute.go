556a3a557a4231abf671ee2aee82b58d6bbc588d
Thomas Jost <thomas.jost@rwth-aachen.de>
2017-12-30 16:39:54 +0100

    Added go raw socket traceroute

--- tls13measurements/traceroute.go
+++ tls13measurements/traceroute.go
@@ -1,15 +1,46 @@
 package tls13measurements
 
 import (
+	"log"
 	"net"
 	"os/exec"
 	"strconv"
 	"strings"
+
+	"github.com/zmap/zgrab/tls13measurements/traceroute"
 )
 
 type Route map[int]string
 
 func TraceRoute(ip net.IP) Route {
+	return TraceRoute_GO(ip)
+}
+
+func TraceRoute_GO(ip net.IP) Route {
+	options := traceroute.TracerouteOptions{}
+	options.SetRetries(0)
+	options.SetMaxHops(30 + 1)
+
+	route := make(Route)
+	res, err := traceroute.Traceroute(ip.String(), &options)
+	if err != nil {
+		log.Print("Traceroute error: ", err)
+	}
+
+	for _, hop := range res.Hops {
+		if hop.Success {
+			route[hop.TTL] = hop.AddressString()
+		}
+	}
+
+	if len(route) == 0 {
+		return nil
+	}
+
+	return route
+}
+
+func TraceRoute_SYS(ip net.IP) Route {
 	out, err := exec.Command("traceroute", "-n", "-N 1", ip.String()).Output()
 	if err != nil {
 		return nil
